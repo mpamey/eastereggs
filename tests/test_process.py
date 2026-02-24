8e0ed2cca0202400f3cf7be7080abdf0bd1afe48
Maxim <maxim.de.mey@ah.nl>
2026-02-24 14:57:28 +0100

    linting

---
+++ tests/test_process.py
@@ -0,0 +1,25 @@
+from src.process import process 
+
+
+def test_addition():
+    # Arrange
+    getal_1 = 2
+    getal_2 = 3
+    
+    validate_sum = 8
+    # Act
+    result = process.addition(getal_1, getal_2)
+    # Assert
+    assert result == validate_sum
+    
+    
+def test_subtraction():
+    # Arrange
+    getal_1 = 5
+    getal_2 = 3
+    
+    validate_subtraction = 7
+    # Act
+    result = process.subtraction(getal_1, getal_2)
+    # Assert
+    assert result == validate_subtraction
